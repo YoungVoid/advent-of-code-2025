6ecf70d783585e6f288ce9037e79d419fad192ee
Nian Vrey <51823073+YoungVoid@users.noreply.github.com>
2025-12-07 16:11:32 +0200

    Day 7 Part 2 in Python

--- Day07/day07.py
+++ Day07/day07.py
@@ -1,4 +1,12 @@
 from rich import print
+import time
+
+class timeline_stats:
+    timeline_splits_value = 0
+    timeline_count = 0
+
+timeline_split_cache = {}
+
 
 def get_data(file_path: str) -> list[str]:
     data = []
@@ -9,32 +17,39 @@ def get_data(file_path: str) -> list[str]:
 
 def run_timeline_and_get_splits_at_end(data: list[str], start_line, start_column) -> int:
     '''
-    run main timeline:
-
-
-    get map
-    get column
+    get map, line, column
     run beam down until end of loop
     if hit splitter
         create left timeline
         create right timeline
         break out of loop since this one wont end at bottom
+    else if hit end, return 1 (base case)
     '''
+    timeline_stats.timeline_count += 1
+    print(timeline_stats.timeline_count,timeline_stats.timeline_splits_value, start_line, start_column)
+    cache_key = f'{start_line}, {start_column}'
+    if cache_key in timeline_split_cache:
+        return timeline_split_cache[cache_key]
 
     timeline_data = data.copy()
     total_splits_at_end = 0
+    column_path = list(zip(*timeline_data))[start_column][start_line:]
+    try:
+        splitter_position = column_path.index('^')
+    except ValueError:
+        # No splitter found, means timeline reaches end - base case for recursion
+        total_splits_at_end = 1
+        timeline_stats.timeline_splits_value += 1
+        
+        timeline_split_cache[cache_key] = total_splits_at_end
+        return total_splits_at_end
+    
+    splitter_line_position_in_main_data = splitter_position + start_line
 
-    for i in range(start_line, len(timeline_data)):
-        if data[i][start_column] == '.':
-            data[i] = data[i][0:start_column] + '|' + data[i][start_column+1:]
-        elif data[i][start_column] == '^':
-            total_splits_at_end += run_timeline_and_get_splits_at_end(timeline_data, i+1, start_column-1) # left timeline
-            total_splits_at_end += run_timeline_and_get_splits_at_end(timeline_data, i+1, start_column+1) # right timeline
-            return total_splits_at_end
-
-
-
-    total_splits_at_end = 1
+    total_splits_at_end += run_timeline_and_get_splits_at_end(timeline_data, splitter_line_position_in_main_data+1, start_column-1) # left timeline
+    total_splits_at_end += run_timeline_and_get_splits_at_end(timeline_data, splitter_line_position_in_main_data+1, start_column+1) # right timeline
+    
+    timeline_split_cache[cache_key] = total_splits_at_end
     return total_splits_at_end
 
 
@@ -43,12 +58,18 @@ def main(file_path: str):
     data = get_data(file_path)
 
     total_splits = 0
-    beam_columns = []
 
     start_column_position = data[0].index('S')
     start_line_position = 1
+    
+    start_time = time.perf_counter()
 
     total_splits = run_timeline_and_get_splits_at_end(data, start_line_position, start_column_position)
+
+    end_time = time.perf_counter()
+
+    elapsed_time = end_time - start_time
+    print(f'Run Time: {elapsed_time // 60}m {elapsed_time%60}s')
 
     print(total_splits)
     
